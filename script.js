527810ae99e8bd72648960c7e138c8b414b38198
DestinySolomon <deesystems247@gmail.com>
2025-08-02 14:20:27 +0100

    Made changes and added dashboard overview and live analytics

--- script.js
+++ script.js
@@ -202,24 +202,21 @@ function validateForm() {
   return true;
 }
 
+const toggleBtn = document.getElementById("darkModeToggle");
+const icon = toggleBtn.querySelector("i");
 
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+  icon.classList.replace("fa-moon", "fa-sun");
+}
 
-  const toggleBtn = document.getElementById('darkModeToggle');
-  const icon = toggleBtn.querySelector('i');
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  const isDark = document.body.classList.contains("dark-mode");
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+  icon.classList.toggle("fa-moon");
+  icon.classList.toggle("fa-sun");
+});
 
-  // Load saved theme
-  if (localStorage.getItem('theme') === 'dark') {
-    document.body.classList.add('dark-mode');
-    icon.classList.replace('fa-moon', 'fa-sun');
-  }
-
-  toggleBtn.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    const isDark = document.body.classList.contains('dark-mode');
-    localStorage.setItem('theme', isDark ? 'dark' : 'light');
-    icon.classList.toggle('fa-moon');
-    icon.classList.toggle('fa-sun');
-  });
-
-  //SideBar
-
+//Dasboard overview
